1190e9ec270411a9bdb40dff7b79dc225b6deb02
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-10-29 20:45:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  arr.map((emp) => {
+    if (emp.profession === "developer") {
+      console.log(
+        `id : ${emp.id}, Name : ${emp.name}, Age : ${emp.age}, Profession : ${emp.profession}`
+      );
+    }
+  });
+}
+
+function PrintDeveloperbyForEach() {
+  arr.forEach((emp) => {
+    if (emp.profession === "developer") {
+      console.log(
+        `id : ${emp.id}, Name : ${emp.name}, Age : ${emp.age}, Profession : ${emp.profession}`
+      );
+    }
+  });
+}
+
+function addData() {
+  const newEmp = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmp);
+  arr.map((emp) => {
+    console.log(
+      `id : ${emp.id}, Name : ${emp.name}, Age : ${emp.age}, Profession : ${emp.profession}`
+    );
+  });
+}
+
+function removeAdmin() {
+  arr
+    .filter((emp) => emp.profession !== "admin")
+    .map((emp) => {
+      console.log(
+        `id : ${emp.id}, Name : ${emp.name}, Age : ${emp.age}, Profession : ${emp.profession}`
+      );
+    });
+}
+
+function concatenateArray() {
+  let friends = [
+    { id: 5, name: "Venkat", age: "19", profession: "developer" },
+    { id: 6, name: "Suresh", age: "21", profession: "developer" },
+    { id: 7, name: "Shanmukh", age: "29", profession: "analyst" },
+  ];
+  let newArray = arr.concat(friends);
+  newArray.map((emp) => {
+    console.log(
+      `id : ${emp.id}, Name : ${emp.name}, Age : ${emp.age}, Profession : ${emp.profession}`
+    );
+  });
+}
